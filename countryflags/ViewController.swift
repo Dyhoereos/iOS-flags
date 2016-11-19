214f3e036b040dc1a1c9a177271c56515b43c3cf
Dyhoereos <dennis.chau@outlook.com>
2016-11-19 00:25:29 -0800

    Pass country codes to new view controller

--- countryflags/ViewController.swift
+++ countryflags/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 import Alamofire
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellReuseIdentifier = "countryIdentifier"
+    
     let url = "https://restcountries.eu/rest/v1/all"
     var countries: [(name: String, code: String)] = []
     override func viewDidLoad() {
@@ -16,7 +20,7 @@ class ViewController: UIViewController {
        
         Alamofire.request(url, method: .get, encoding: JSONEncoding.default)
                 .responseJSON { response in
-                    print(response)
+                    //print(response)
 
                     if let JSON = response.result.value {
                         let jsonArray = JSON as? [[String:Any]]
@@ -24,8 +28,10 @@ class ViewController: UIViewController {
                             let cname = item["name"]! as! String
                             let ccode = item["alpha2Code"]! as! String
                             self.countries += [(name: cname, code: ccode.lowercased())]
+                            self.tableView.insertRows(at: [IndexPath(row: self.countries.count-1, section: 0)], with: .automatic)
                            
                         }
+                        
                     }
             }
     }
@@ -35,5 +41,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Getting the right element
+        let country = countries[indexPath.row]
+        
+        // Instantiate a cell
+        let cell = UITableViewCell(style: .default, reuseIdentifier: cellReuseIdentifier)
+        
+        // Adding the right informations
+        cell.textLabel?.text = country.name
+        
+        // Returning the cell
+        return cell
+    }
 
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        // pass country code to FlagViewController
+        let flagVC = FlagViewController()
+        flagVC.country = countries[indexPath.row].code
+        navigationController?.pushViewController(flagVC, animated: true)    }
 }
